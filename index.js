8b73f4192e30b51e16cf249860ff22a14ca910fb
MSimnes <106779624+MSimnes@users.noreply.github.com>
2023-03-07 14:28:06 -0800

    2 files modified, 3 added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // run `node index.js` in the terminal
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
-app.
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
 
 console.log(`Hello Node.js v${process.versions.node}!`);
