c617145f971a5baab225cf40101d356a5ed55a7a
MSimnes <106779624+MSimnes@users.noreply.github.com>
2023-03-07 12:47:53 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StackBlitz-WebContainer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ycwpom)
---
+++ index.js
@@ -0,0 +1,8 @@
+// run `node index.js` in the terminal
+const express = require('express');
+const app = express();
+
+app.
+
+
+console.log(`Hello Node.js v${process.versions.node}!`);
